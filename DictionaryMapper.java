00955d14e3ef6af1e96a388303827e9563c5987b
SandyarathiDas <sandyarathi1212@gmail.com>
2016-03-07 02:28:42 -0800

    FixedPOS

--- DictionaryMapper.java
+++ DictionaryMapper.java
@@ -28,7 +28,7 @@ public class DictionaryMapper  extends Mapper<Text, Text, Text, Text> {
                         }
                         
                     }*/
-                String partsOfSpeech = value.toString().substring(value.toString().lastIndexOf('['),value.toString().length());
+                String partsOfSpeech = value.toString().substring(value.toString().lastIndexOf('[')+1,value.toString().length()-1);
                 String translations= value.toString().substring(0,value.toString().lastIndexOf('[')-1 );
                 if(valid(partsOfSpeech)){
                     String keyMap= key + " : ["+partsOfSpeech+"]";
